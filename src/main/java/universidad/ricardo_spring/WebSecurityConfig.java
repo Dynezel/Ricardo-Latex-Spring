ee2753400c8574e23e029de9676ecfc14c76df7a
Dynezel <dylan50xd@gmail.com>
2024-07-27 20:57:03 -0300

    cambios autenticacion

--- src/main/java/universidad/ricardo_spring/WebSecurityConfig.java
+++ src/main/java/universidad/ricardo_spring/WebSecurityConfig.java
@@ -58,7 +58,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements W
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("ricardo-latex-react.vercel.app");
+        config.addAllowedOrigin("https://ricardo-latex-react.vercel.app");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.setAllowCredentials(true);
