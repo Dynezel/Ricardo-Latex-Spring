14708553ace7c13f1f3d108c2d154f0ec1b7bc32
Dynezel <dylan50xd@gmail.com>
2024-07-27 15:02:03 -0300

    cambios autenticacion

--- src/main/java/universidad/ricardo_spring/controladores/AuthController.java
+++ src/main/java/universidad/ricardo_spring/controladores/AuthController.java
@@ -1,10 +1,12 @@
 package universidad.ricardo_spring.controladores;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,9 @@ import universidad.ricardo_spring.entidades.LoginRequest;
 import universidad.ricardo_spring.entidades.Usuario;
 import universidad.ricardo_spring.servicios.UsuarioDetalles;
 import universidad.ricardo_spring.servicios.UsuarioService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/auth")
@@ -34,6 +39,14 @@ public class AuthController {
         );
         SecurityContextHolder.getContext().setAuthentication(authentication);
         return usuarioService.loadUserByUsername(loginRequest.getUsername());
+    }
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return ResponseEntity.ok("Logout successful");
     }
 }
 
