4303d6403cf2a2a8a43881f0fa93e69c103950c9
Dynezel <dylan50xd@gmail.com>
2024-07-31 17:43:34 -0300

    Cambios autorizacion

--- src/main/java/universidad/ricardo_spring/WebSecurityConfig.java
+++ src/main/java/universidad/ricardo_spring/WebSecurityConfig.java
@@ -95,7 +95,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements W
                         .invalidateHttpSession(true)
                         .deleteCookies("JSESSIONID")
                         .permitAll()
-                )
+                );
     }
 
     @Bean
