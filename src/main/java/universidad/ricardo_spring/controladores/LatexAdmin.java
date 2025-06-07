876048d8a62a9d67ff81bcfc8935a160fbd101dd
Dynezel <dylan50xd@gmail.com>
2025-06-07 12:25:41 -0300

    cambios conexion

--- src/main/java/universidad/ricardo_spring/controladores/LatexAdmin.java
+++ src/main/java/universidad/ricardo_spring/controladores/LatexAdmin.java
@@ -62,7 +62,7 @@ public class LatexAdmin {
     }
     @PostMapping("/verify-code/6f9d3c2e-1a5b-4d6e-90bc-89f3c4e7b1a2-47e1c9d6f0a5b8d7e6c9a3b2c1f8e0d9")
     public ResponseEntity<Boolean> verifyCreationCode(@RequestBody Map<String, String> requestBody) {
-        String receivedCode = requestBody.get("code").trim();
+        String receivedCode = requestBody.get("creationCode").trim();
 
         if (codigoCreacion.equals(receivedCode)) {
             System.out.println("Codigo recibido: " + receivedCode);
